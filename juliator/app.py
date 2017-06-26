3399d684a7c528d338b381d87d6353dd55123f13
Marc Culler <none@none>
2017-06-26 15:15:24 -0500

    Added GPL license.

--- juliator/app.py
+++ juliator/app.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright© 2012-2017 by Marc Culler and others.
+# This file is part of Juliator.
+#
+# Juliator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Juliator is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Juliator.  If not, see <http://www.gnu.org/licenses/>.
+
 import juliator
 
 def main():
